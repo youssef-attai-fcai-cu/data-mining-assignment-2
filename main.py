3e5e528519c8ce09f8f830ed35e0f7c110720bfc
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-04-20 10:25:19 +0200

    Plot results

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox
+import random
 import pandas as pd
 import numpy as np
-import random
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class K_Means:
+    @staticmethod
+    def start(filename, percentage, k):
+        """Run clustering on the data."""
+        try:
+            data = CSVReader.read_data(filename, percentage)
+
+            # Detect and remove outliers
+            points, outliers = OutlierCalculator.iqr(
+                data['IMDB Rating']
+            )
+
+            # Convert relevant columns to numpy array for clustering
+            features = points.values.reshape(-1, 1)
+
+            k_means = K_Means(k=k)
+
+            # Fit the data (without outliers) to the model
+            k_means.fit(features)
+
+            # Output cluster contents
+            results = {}
+            for i in range(k):
+                cluster_results = [jj for j, jj in enumerate(
+                    points.values) if k_means.predict(features[j]) == i]
+
+                results[f"Cluster {i+1}"] = cluster_results
+
+            return results, outliers
+
+        except Exception as e:
+            print("Something went wrong :(")
+            print(e)
+            messagebox.showerror("Error", "Something went wrong :(")
+            return None, None
+
     def __init__(self, k=3, max_number_of_iterations=1000):
-        self.k = k
         self.max_iter = max_number_of_iterations
-
-    def __random_item(self, data):
-        """Choose a random data point as a centroid."""
-        return data[random.randint(0, len(data)-1)]
-
-    def __euclidean_distance(self, features, centroid):
-        """Calculate the Euclidean distance between a data point and a centroid."""
-        return np.linalg.norm(
-            features - self.centroids[centroid])
+        self.k = k
 
     def fit(self, data):
         """Fit the data to the model."""
@@ -51,7 +79,7 @@ class K_Means:
             # Copy old centroids
             old_centroids = {i: self.centroids[i] for i in self.centroids}
 
-            # Update centroids by taking the average of all points in the cluster
+            # Update centroids (average of all points in the cluster)
             for _class in self.classes:
                 # Check if cluster is not empty
                 if self.classes[_class]:
@@ -70,73 +98,87 @@ class K_Means:
             if optimum:
                 break
 
-    def __converged(self, old_centroid, new_centroid):
-        """Check if the centroids have converged."""
-        return np.sum((new_centroid-old_centroid)/old_centroid*100.0) > 0.001
-
     def predict(self, data):
         """Predict the cluster of a new data point."""
         distances = [np.linalg.norm(data - self.centroids[centroid])
                      for centroid in self.centroids]
-        classification = distances.index(min(distances))
-        return classification
+        _class = distances.index(min(distances))
+        return _class
+
+    def __converged(self, old_centroid, new_centroid):
+        """
+        Check if the centroids have converged.
+
+        If the centroids have moved more than 0.001%, they have not converged.
+        Otherwise, they have converged.
+
+        Calculated as the sum of the percentage change in each dimension.
+        """
+        return np.sum((new_centroid-old_centroid)/old_centroid*100.0) > 0.001
 
     def __calculate_average(self, _class):
         """Calculate the average of all points in a cluster."""
         return sum(
             self.classes[_class]) / len(self.classes[_class])
 
+    def __random_item(self, data):
+        """Choose a random data point as a centroid."""
+        return data[random.randint(0, len(data)-1)]
 
-def read_data(filename, percentage):
-    """Read data from a CSV file."""
-    data = pd.read_csv(filename)
-    data = data.sample(frac=percentage, random_state=1)
-    return data
-
-
-def detect_outliers(data):
-    """Detect and remove outliers from data."""
-    # Detect outliers using IQR
-    quantile25 = data.quantile(0.25)
-    quantile75 = data.quantile(0.75)
-    iqr = quantile75 - quantile25
-    outliers = data[(data < (quantile25 - 1.5 * iqr)) |
-                    (data > (quantile75 + 1.5 * iqr))]
-
-    # Remove outliers from data
-    points_without_outliers = data[~data.isin(outliers)].dropna()
-
-    return points_without_outliers, outliers
+    def __euclidean_distance(self, features, centroid):
+        """Calculate the Euclidean distance between a point and a centroid."""
+        return np.linalg.norm(
+            features - self.centroids[centroid])
 
 
-def run_clustering(filename, percentage, k):
-    """Run clustering on the data."""
-    try:
-        data = read_data(filename, percentage)
+class CSVReader:
+    @staticmethod
+    def read_data(filename, percentage):
+        """Read data from a CSV file."""
+        data = pd.read_csv(filename)
+        data = data.sample(frac=percentage, random_state=1)
+        return data
 
-        # Detect and remove outliers
-        points_without_outliers, outliers = detect_outliers(
-            data['IMDB Rating'])
 
-        # Convert relevant columns to numpy array for clustering
-        features = points_without_outliers.values.reshape(-1, 1)
+class OutlierCalculator:
+    @staticmethod
+    def z_score(data):
+        """Detect and remove outliers from data."""
 
-        k_means = K_Means(k=k)
-        k_means.fit(features)
+        # Calculate Z-score
+        z = np.abs((data - data.mean()) / data.std())
 
-        # Output cluster contents
-        results = {}
-        for i in range(k):
-            cluster_results = [row for j, row in enumerate(
-                points_without_outliers.values) if k_means.predict(features[j]) == i]
-            results[f"Cluster {i+1}"] = cluster_results
+        # Calculate outliers using Z-score
+        outliers = data[z > 3]
 
-        return results, outliers
+        # Remove outliers from data
+        data_in_outliers = data.isin(outliers)
+        data_not_in_outliers = data[~data_in_outliers]
+        data_not_in_outliers_without_nan = data_not_in_outliers.dropna()
 
-    except Exception as e:
-        print("Something went wrong :(")
-        print(e)
-        return None, None
+        return data_not_in_outliers_without_nan, outliers
+
+    @staticmethod
+    def iqr(data):
+        """Detect and remove outliers from data."""
+
+        # Calculate IQR
+        quantile25 = data.quantile(0.25)
+        quantile75 = data.quantile(0.75)
+        iqr = quantile75 - quantile25
+
+        # Calculate outliers using IQR
+        data_less_than_lower_bound = data < (quantile25 - 1.5 * iqr)
+        data_greater_than_upper_bound = data > (quantile75 + 1.5 * iqr)
+        outliers = data[data_less_than_lower_bound |
+                        data_greater_than_upper_bound]
+
+        # Remove outliers from data
+        data_in_outliers = data.isin(outliers)
+        data_not_in_outliers = data[~data_in_outliers]
+        data_not_in_outliers_without_nan = data_not_in_outliers.dropna()
+
+        return data_not_in_outliers_without_nan, outliers
 
 
 class GUI:
@@ -144,29 +186,42 @@ class GUI:
         self.root = tk.Tk()
         self.root.title("IMDb Movies Clustering")
 
+        # CSV path
         self.csv_path_label = tk.Label(self.root, text="CSV Path:")
-        self.csv_path_label.grid(row=0, column=0)
-
         self.csv_path_entry = tk.Entry(self.root, width=30)
-        self.csv_path_entry.grid(row=0, column=1)
 
+        # Choose CSV button
         self.choose_csv_button = tk.Button(
             self.root, text="Choose CSV", command=lambda: self.choose_csv())
-        self.choose_csv_button.grid(row=0, column=2)
 
+        # Percentage of data to use
         self.percentage_label = tk.Label(self.root, text="Percentage (0-100):")
-        self.percentage_label.grid(row=1, column=0)
         self.percentage_entry = tk.Entry(self.root, width=10)
-        self.percentage_entry.grid(row=1, column=1)
 
+        # k value
         self.k_label = tk.Label(self.root, text="k:")
-        self.k_label.grid(row=2, column=0)
         self.k_entry = tk.Entry(self.root, width=10)
-        self.k_entry.grid(row=2, column=1)
 
+        # Run button
         self.run_button = tk.Button(
             self.root, text="Run", command=lambda: self.run())
-        self.run_button.grid(row=3, column=1)
+
+        # Matplotlib figure
+        self.figure = Figure(figsize=(5, 4), dpi=100)
+        self.plot = self.figure.add_subplot(111)
+        self.canvas = FigureCanvasTkAgg(self.figure, self.root)
+        self.canvas.draw()
+
+        # Place widgets
+        self.csv_path_label.grid(row=0, column=0)
+        self.csv_path_entry.grid(row=0, column=1)
+        self.choose_csv_button.grid(row=0, column=2)
+        self.percentage_label.grid(row=1, column=0)
+        self.percentage_entry.grid(row=1, column=1)
+        self.k_label.grid(row=2, column=0)
+        self.k_entry.grid(row=2, column=1)
+        self.run_button.grid(row=3, column=0)
+        self.canvas.get_tk_widget().grid(row=4, column=0)
 
         self.root.mainloop()
 
@@ -183,30 +238,32 @@ class GUI:
 
             k = int(self.k_entry.get())
 
-            cluster_contents, outliers = run_clustering(
-                self.csv_path_entry.get(), percentage, k)
-            output_window = tk.Toplevel()
-            output_window.title("Clustering Output")
+            csv_path = self.csv_path_entry.get()
 
-            # Create a Text widget to display cluster contents
-            cluster_text = tk.Text(output_window, width=50, height=20)
-            cluster_text.pack(fill=tk.BOTH, expand=True)
+            clustering_result, outliers = K_Means.start(
+                csv_path, percentage, k)
 
-            # Output cluster contents
-            cluster_text.insert(tk.END, "Cluster contents:\n\n")
-            for cluster, contents in cluster_contents.items():
-                cluster_text.insert(tk.END, f"{cluster}:\n")
-                for content in contents:
-                    cluster_text.insert(tk.END, f"{content}\n")
-                cluster_text.insert(tk.END, "\n")
+            print(F"Clusters: {clustering_result}")
+            print(F"Outliers: {outliers}")
 
-            # Create a Text widget to display outliers
-            outliers_text = tk.Text(output_window, width=50, height=5)
-            outliers_text.pack(fill=tk.BOTH, expand=True)
+            # Display cluster contents in Matplotlib
+            self.plot.clear()
+            for cluster, points_in_cluster in clustering_result.items():
+                self.plot.scatter(
+                    np.arange(len(points_in_cluster)),
+                    points_in_cluster,
+                    label=cluster)
+            self.plot.legend()
+            self.canvas.draw()
 
-            # Output outliers
-            outliers_text.insert(tk.END, "Outliers:\n\n")
-            outliers_text.insert(tk.END, outliers.to_string(index=False))
+            # Color outliers red
+            if outliers is not None:
+                self.plot.scatter(
+                    np.arange(len(outliers)),
+                    outliers, color='red', label='Outliers'
+                )
+                self.plot.legend()
+                self.canvas.draw()
 
         except ValueError:
             messagebox.showerror(
@@ -217,6 +274,5 @@ class GUI:
 
 
 if __name__ == "__main__":
-    # main()
     gui = GUI()
     gui.main()
